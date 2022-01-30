3a174707679692715c03365fdd9b1408d39c593d
Thiago <thiagomf@motorola.com>
2022-01-30 12:46:03 -0300

    add chakra ui and its configurations

---
+++ src/styles/theme.ts
@@ -0,0 +1,27 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export const theme = extendTheme({
+  colors: {
+    gray: {
+      900: "#47585B",
+      700: "#999999",
+      500: "#DADADA",
+      300: "#F5F8FA",
+    },
+    yellow: {
+      500: "FFBA08",
+    },
+  },
+  fonts: {
+    body: "Poppins",
+    heading: "Poppins",
+  },
+  styles: {
+    global: {
+      body: {
+        bg: "gray.300",
+        color: "gray.900",
+      },
+    },
+  },
+});
